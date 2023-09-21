edad2737f20a45b26d152fee0300ab0bf6b8f87a
marcygon <marciasofia49@gmail.com>
2023-09-21 12:37:15 +0200

    added buttons

--- src/components/table-mixer/TableMixer.jsx
+++ src/components/table-mixer/TableMixer.jsx
@@ -21,14 +21,22 @@ function tableMixer() {
 
   return (
     <>
-        <button id="btn1" className="btn topBtn col1" onClick={() => audio1.play()}/>
-        <button id="btn2" className="btn topBtn col2" onClick={() => audio2.play()}/>
-        <button id="btn3" className="btn topBtn col3" onClick={() => audio3.play()}/>
-        <button id="btn4" className="btn topBtn col4" onClick={() => audio4.play()}/>
-        <button id="btn5" className="btn bottomBtn col1" onClick={() => audio5.play()}/>
-        <button id="btn6" className="btn bottomBtn col2" onClick={() => audio6.play()}/>
-        <button id="btn7" className="btn bottomBtn col3" onClick={() => audio7.play()}/>
-        <button id="btn8" className="btn bottomBtn col4" onClick={() => audio8.play()}/>
+        <button id="btn1" className="btn row1 col1" onClick={() => audio1.play()}/>
+        <button id="btn2" className="btn row1 col2" onClick={() => audio2.play()}/>
+        <button id="btn3" className="btn row1 col3" onClick={() => audio3.play()}/>
+        <button id="btn4" className="btn row1 col4" onClick={() => audio4.play()}/>
+        <button id="btn5" className="btn row2 col1" onClick={() => audio5.play()}/>
+        <button id="btn6" className="btn row2 col2" onClick={() => audio6.play()}/>
+        <button id="btn7" className="btn row2 col3" onClick={() => audio7.play()}/>
+        <button id="btn8" className="btn row2 col4" onClick={() => audio8.play()}/>
+        <button id="btn9" className="btn row3 col1" onClick={() => audio1.play()}/>
+        <button id="btn10" className="btn row3 col2" onClick={() => audio2.play()}/>
+        <button id="btn11" className="btn row3 col3" onClick={() => audio3.play()}/>
+        <button id="btn12" className="btn row3 col4" onClick={() => audio4.play()}/>
+        <button id="btn13" className="btn row4 col1" onClick={() => audio5.play()}/>
+        <button id="btn14" className="btn row4 col2" onClick={() => audio6.play()}/>
+        <button id="btn15" className="btn row4 col3" onClick={() => audio7.play()}/>
+        <button id="btn16" className="btn row4 col4" onClick={() => audio8.play()}/>
     </>
   )
 }
